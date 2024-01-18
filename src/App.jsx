6f5b820e858a8a077c9818eb02767dcd761323cd
unknown <monicaimbrea7@gmail.com>
2024-01-18 17:20:18 +0200

    added header component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes></Routes>
     </Router>
   );
