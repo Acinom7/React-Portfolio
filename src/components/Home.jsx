7f181c6d6a50a8e1c530170a061e59f29d0e827d
unknown <monicaimbrea7@gmail.com>
2024-01-18 22:16:04 +0200

    changes to components folder

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import myPhoto from "../../public/image.jpeg";
+
+function Hero() {
+  return (
+    <section class="bg-light text-dark p-5 text-center">
+      <div className="container">
+        <div className="d-sm-flex justify-content-around align-items-center">
+          <div>
+            <h1>Hello, I'm Monica</h1>
+            <p className="lead mb-3">Frontend Developer</p>
+            <div className="icon mb-4">
+              <a href="https://www.twitter.com">
+                <i className="fa fa-twitter fa-2x icon-spacing"></i>
+              </a>
+              <a href="https://www.linkedin.com">
+                <i className="fa fa-linkedin fa-2x icon-spacing"></i>
+              </a>
+              <a href="https://github.com">
+                <i className="fa fa-github fa-2x"></i>
+              </a>
+            </div>
+          </div>
+          <img
+            className="img-fluid w-25 rounded"
+            id="myPhoto"
+            src={myPhoto}
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
