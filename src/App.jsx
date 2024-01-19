bd7c46ca13df3883e49c3692f7da5be45cc4947c
unknown <monicaimbrea7@gmail.com>
2024-01-19 19:06:35 +0200

    removed path in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="projects" element={<Projects />} />
       </Routes>
     </Router>
   );
