feef9b3375fa97e26550f32567612a90123537a1
unknown <monicaimbrea7@gmail.com>
2024-01-19 14:06:06 +0200

    added function to about page

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h2 class="text-center py-5 mt-5">About Me</h2>
+      <div className="container bg-light text-dark p-5 text-center w-50 rounded">
+        <p class="lead">
+          I'm a front-end developer who specializes in creating visually
+          appealing, responsive websites. My skills in HTML, CSS, and JavaScript
+          allow me to transform designs into user-friendly web interfaces. I
+          thrive on problem-solving and stay up-to-date with the latest web
+          development trends to deliver top-notch results.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
