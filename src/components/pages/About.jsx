086d809742791b6aeab5681d0bf87a9df39d9f02
unknown <monicaimbrea7@gmail.com>
2024-01-19 19:04:10 +0200

    made changes to style

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h2 class="text-center py-5 mt-5">About Me</h2>
-      <div className="container bg-light text-dark p-5 text-center w-50 rounded">
+    <div className="py-5">
+      <h2 class="text-center py-5">About Me</h2>
+      <div className="container bg-light text-dark p-5 text-center w-75 rounded">
         <p class="lead">
           I'm a front-end developer who specializes in creating visually
           appealing, responsive websites. My skills in HTML, CSS, and JavaScript
