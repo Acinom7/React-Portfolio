022ee7e9ca7a4d9e7c7cb5d105fedf939674aaa4
unknown <monicaimbrea7@gmail.com>
2024-01-19 12:33:44 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
